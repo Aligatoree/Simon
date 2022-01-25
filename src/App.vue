b5063875b0b3b2f56a983c7d7ba36e7bd6b6af8c
Aligatoree <85631225+Aligatoree@users.noreply.github.com>
2022-01-25 22:16:21 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Saimon />
+  </div>
+</template>
+
+<script>
+import Saimon from "./components/Saimon.vue";
+
+export default {
+  name: "App",
+  components: {
+    Saimon,
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: rgb(255, 255, 225);
+}
+#app {
+  width: 60%;
+  min-width: 300px;
+  margin: 50px auto;
+}
+</style>
